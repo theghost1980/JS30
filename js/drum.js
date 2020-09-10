4985044ef63649f47146fd00a0275da1cd63a2d1
theghost1980 <saturnob612@gmail.com>
2020-09-09 20:25:20 -0400

    using arrays

--- js/drum.js
+++ js/drum.js
@@ -1,52 +1,48 @@
 //credits on some files Audio files. S1 Artist Series Sample Pack!
 //Artist:(@symbolycone)downloaded from: https://cymatics.fm/blogs/production/free-drum-kits
 //cymbal sound from: http://www.dashingstrike.com/Automato/games/ATITD/sounds/cymbals.wav
+//drum bg from: https://wall.alphacoders.com/by_sub_category.php?id=173452&name=Drums+Wallpapers
 
 document.onkeypress = check_keyPress;
 //some vars
-var a = document.getElementById("a");
-var s = document.getElementById("s");
-var d = document.getElementById("d");
-var f = document.getElementById("f");
-var g = document.getElementById("g");
+//using array
+var p_array = [];
+var drum_list = document.querySelectorAll('.p_ar');
+drum_list.forEach(function(x){
+    p_array.push(x);
+});
+
 var snare = new Audio('./media-images/sounds/snare.wav');
 var hihat = new Audio('./media-images/sounds/hihat.wav');
 var kick = new Audio('./media-images/sounds/kick.wav');
 var bell = new Audio('./media-images/sounds/bell.wav');
 var cymbal = new Audio('./media-images/sounds/cymbal.wav');
+var audio_ar = [snare,hihat,kick,bell,cymbal];
 
 function check_keyPress(){
     let evt = this.event || window.event;
-    // alert("Code: " + evt.keyCode);
+    let ind = 0;
     if (evt.keyCode == 97){
-        a.style.color = "red";
-        snare.play();
-        setTimeout(turn_off,600,"a");
+        ind = 0;
     }
     if (evt.keyCode == 115){
-        s.style.color = "red";
-        hihat.play();
-        setTimeout(turn_off,600,"s");
+        ind = 1;
     }
     if (evt.keyCode == 100){
-        d.style.color = "red";
-        kick.play();
-        setTimeout(turn_off,600,"d");
+        ind = 2;
     }
     if (evt.keyCode == 102){
-        f.style.color = "red";
-        bell.play();
-        setTimeout(turn_off,600,"f");
+        ind = 3;
     }
     if (evt.keyCode == 103){
-        g.style.color = "red";
-        cymbal.play();
-        setTimeout(turn_off,600,"g");
+        ind = 4;
     }
+    //with array
+    p_array[ind].style.color = "red";
+    audio_ar[ind].play();
+    setTimeout(turn_off,600,ind);
 }
 
-
 function turn_off(k){
-    // alert(k);
-    document.getElementById(k).style.color = "black";
+    p_array[k].style.color = "black";
 }
